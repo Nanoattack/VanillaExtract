3ab489dc6a3f8f5355f2d534bd86aec166a57b10
Nanoattack <Totallynotarivenmain@gmail.com>
2021-10-01 10:40:30 +0200

    Rice plantable on water

--- src/main/java/com/nano/extract/item/custom/WaterCropBlockItem.java
+++ src/main/java/com/nano/extract/item/custom/WaterCropBlockItem.java
@@ -1,18 +1,9 @@
 package com.nano.extract.item.custom;
 
-import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.IBucketPickupHandler;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.fluid.Fluid;
-import net.minecraft.fluid.Fluids;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.stats.Stats;
-import net.minecraft.tags.FluidTags;
+import net.minecraft.item.*;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
@@ -21,25 +12,32 @@ import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 
 public class WaterCropBlockItem extends BlockItem
-
 {
-    private final Fluid content;
-    {
-
-    }
-
     public WaterCropBlockItem(Block block, Properties properties)
     {
         super(block, properties);
-        this.content = null;
     }
-/*
+
+    public ActionResultType useOn(ItemUseContext context) {
+        ActionResultType actionresulttype = this.place(new BlockItemUseContext(context));
+        if(context.getLevel().getBlockState(context.getClickedPos().above()).getBlock().is(Blocks.WATER)) {
+            BlockItemUseContext newContext = new BlockItemUseContext(context.getPlayer(),
+                    context.getHand(), context.getItemInHand(),
+                    new BlockRayTraceResult(context.getClickLocation(), context.getClickedFace(),
+                            context.getClickedPos().relative(context.getClickedFace(), 2), true));
+
+            actionresulttype = this.place(newContext);
+        }
+
+        return !actionresulttype.consumesAction() && this.isEdible() ? this.use(context.getLevel(),
+                context.getPlayer(), context.getHand()).getResult() : actionresulttype;
+    }
+
     @Override
     public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
         ItemStack itemstack = player.getItemInHand(hand);
-        RayTraceResult raytraceresult = getPlayerPOVHitResult(world, player, this.content == Fluids.EMPTY ? RayTraceContext.FluidMode.SOURCE_ONLY : RayTraceContext.FluidMode.NONE);
-        ActionResult<ItemStack> ret = net.minecraftforge.event.ForgeEventFactory.onBucketUse(player, world, itemstack, raytraceresult);
-        if (ret != null) return ret;
+        RayTraceResult raytraceresult = getPlayerPOVHitResult(world, player, RayTraceContext.FluidMode.SOURCE_ONLY);
+
         if (raytraceresult.getType() == RayTraceResult.Type.MISS) {
             return ActionResult.pass(itemstack);
         } else if (raytraceresult.getType() != RayTraceResult.Type.BLOCK) {
@@ -49,45 +47,17 @@ public class WaterCropBlockItem extends BlockItem
             BlockPos blockpos = blockraytraceresult.getBlockPos();
             Direction direction = blockraytraceresult.getDirection();
             BlockPos blockpos1 = blockpos.relative(direction);
+
             if (world.mayInteract(player, blockpos) && player.mayUseItemAt(blockpos1, direction, itemstack)) {
-                if (this.content == Fluids.EMPTY) {
-                    BlockState blockstate1 = world.getBlockState(blockpos);
-                    if (blockstate1.getBlock() instanceof IBucketPickupHandler) {
-                        Fluid fluid = ((IBucketPickupHandler)blockstate1.getBlock()).takeLiquid(world, blockpos, blockstate1);
-                        if (fluid != Fluids.EMPTY) {
-                            player.awardStat(Stats.ITEM_USED.get(this));
-
-                            SoundEvent soundevent = this.content.getAttributes().getFillSound();
-                            if (soundevent == null) soundevent = fluid.is(FluidTags.LAVA) ? SoundEvents.BUCKET_FILL_LAVA : SoundEvents.BUCKET_FILL;
-                            player.playSound(soundevent, 1.0F, 1.0F);
-                            ItemStack itemstack1 = DrinkHelper.createFilledResult(itemstack, player, new ItemStack(fluid.getBucket()));
-                            if (!world.isClientSide) {
-                                CriteriaTriggers.FILLED_BUCKET.trigger((ServerPlayerEntity)player, new ItemStack(fluid.getBucket()));
-                            }
-
-                            return ActionResult.sidedSuccess(itemstack1, world.isClientSide());
-                        }
-                    }
-
-                    return ActionResult.fail(itemstack);
-                } else {
-                    BlockState blockstate = world.getBlockState(blockpos);
-                    BlockPos blockpos2 = canBlockContainFluid(world, blockpos, blockstate) ? blockpos : blockpos1;
-                    if (this.emptyBucket(player, world, blockpos2, blockraytraceresult)) {
-                        this.checkExtraContent(world, itemstack, blockpos2);
-                        if (player instanceof ServerPlayerEntity) {
-                            CriteriaTriggers.PLACED_BLOCK.trigger((ServerPlayerEntity)player, blockpos2, itemstack);
-                        }
-
-                        player.awardStat(Stats.ITEM_USED.get(this));
-                        return ActionResult.sidedSuccess(this.getEmptySuccessItem(itemstack, player), world.isClientSide());
-                    } else {
-                        return ActionResult.fail(itemstack);
-                    }
-                }
+                this.place(new BlockItemUseContext(player, hand, itemstack, new BlockRayTraceResult(raytraceresult.getLocation(),
+                        direction, blockpos1, true)));
+                return ActionResult.success(itemstack);
             } else {
+                System.out.println("Clicked 4");
                 return ActionResult.fail(itemstack);
             }
         }
-    }*/
+    }
 }
+
+//Thank you KaupenJoe :prayge:
